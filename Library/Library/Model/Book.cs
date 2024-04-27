edcd4d234e2cf376f5d32811d8202b1b37d294a5
khalil <mahmoud.khalil.luxor.edu.eg>
2024-04-27 14:54:36 +0300

    Context added

---
+++ Library/Library/Model/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Book
+    {
+        [Key,Required]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string author {  get; set; }
+        public string Publisher {  get; set; }
+        public Book()
+        {
+
+        }
+        
+
+    }
+}
---
+++ Library/Library/Model/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Context :DbContext
+    {
+        public Context():base("")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
